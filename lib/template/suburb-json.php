acb4d994952914ed7a1944ced41c4c12fd9c8c8d
Marc Kannegiesser <kannegiesser@synyx.de>
2012-10-31 14:51:44 +0100

    also returning place-id with suburb-result fixes #2733

--- lib/template/suburb-json.php
+++ lib/template/suburb-json.php
@@ -5,7 +5,8 @@ $aFilteredPlaces = array();
 foreach ($aParentOfLines as $aAddressLine) {
     $aPlace = array(
         'osm_id' => $aAddressLine['osm_id'],
-        'display_name' => trim($aAddressLine['localname']) ? $aAddressLine['localname'] : 'No Name',
+        'place_id'=>$aAddressLine['place_id'],
+				'display_name' => trim($aAddressLine['localname']) ? $aAddressLine['localname'] : 'No Name',
         'lat' => $aAddressLine['lat'],
         'lon' => $aAddressLine['lon'],
         'short_name' => $aPointDetails['localname']

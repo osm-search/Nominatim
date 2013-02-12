ff61b90bafb8ee9e222dea98034ea46e86c62005
Brian Quinion <openstreetmap@brian.quinion.co.uk>
2013-02-12 13:28:07 +0000

    another int => bigint issue in addressline

--- sql/functions.sql
+++ sql/functions.sql
@@ -2321,7 +2321,7 @@ DROP TYPE addressline CASCADE;
 create type addressline as (
   place_id BIGINT,
   osm_type CHAR(1),
-  osm_id INTEGER,
+  osm_id BIGINT,
   name HSTORE,
   class TEXT,
   type TEXT,

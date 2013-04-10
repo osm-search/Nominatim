2f88e7ceabf900dcb262dd80c41c4d716da1f5a0
Sarah Hoffmann <lonvia@denofr.de>
2013-04-10 19:50:38 +0200

    adapt status page to new word table layout

--- website/status.php
+++ website/status.php
@@ -10,7 +10,7 @@
 		exit;
 	}
 
-	$iWordID = $oDB->getOne("select word_id,word_token, word, class, type, location, country_code, operator, search_name_count from word where word_token in (' a')");
+	$iWordID = $oDB->getOne("select word_id,word_token, word, class, type, country_code, operator, search_name_count from word where word_token in (' a')");
 	if (PEAR::isError($iWordID))
 	{
 		echo "ERROR: Query failed";

f56cae42913606ca9893983e3b62199a7f68b8b9
Brian Quinion <brian.quinion@mapquest.com>
2014-01-23 16:06:43 +0000

    validate postgresql module / functions

--- website/status.php
+++ website/status.php
@@ -10,6 +10,18 @@
 		exit;
 	}
 
+	$sStandardWord = $oDB->getOne("select make_standard_name('a')");
+	if (PEAR::isError($sStandardWord))
+	{
+		echo "ERROR: Module failed";
+		exit;
+	}
+	if ($sStandardWord != 'a')
+	{
+		echo "ERROR: Transliteration failed";
+		exit;
+	}
+
 	$iWordID = $oDB->getOne("select word_id,word_token, word, class, type, country_code, operator, search_name_count from word where word_token in (' a')");
 	if (PEAR::isError($iWordID))
 	{

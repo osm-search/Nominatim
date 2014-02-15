29dfc0c259f55430229afea599913eeae47f9a30
Sarah Hoffmann <lonvia@denofr.de>
2014-02-15 18:49:05 +0100

    allow to pass an additional config file via NOMINATIM_SETTINGS variable to command line tools
    needed for test environment

--- lib/init-cmd.php
+++ lib/init-cmd.php
@@ -1,4 +1,9 @@
 <?php
+	if (file_exists(getenv('NOMINATIM_SETTINGS')))
+	{
+		echo 'Using local config: '.getenv('NOMINATIM_SETTINGS');
+		require_once(getenv('NOMINATIM_SETTINGS'));
+	}
 
 	require_once('init.php');
 	require_once('cmd.php');

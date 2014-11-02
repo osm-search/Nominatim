4b8632d1e563f63591e33f8561271552d074a5e2
Sarah Hoffmann <lonvia@denofr.de>
2014-11-02 19:06:01 +0100

    transliterate precomputed house numbers

--- data/words.sql
+++ data/words.sql
@@ -49637,7 +49637,7 @@ kein	100
 
 select count(make_keywords(v)) from (select distinct svals(name) as v from place) as w where v is not null;
 select count(make_keywords(v)) from (select distinct postcode as v from place) as w where v is not null;
-select count(getorcreate_housenumber_id(v)) from (select distinct housenumber as v from place where housenumber is not null) as w;
+select count(getorcreate_housenumber_id(make_standard_name(v))) from (select distinct housenumber as v from place where housenumber is not null) as w;
 
 -- copy the word frequencies
 update word set search_name_count = count from word_frequencies wf where wf.word_token = word.word_token;

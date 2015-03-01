dee064554878cc258cafbd027d8ce93d66513798
Andreas Hubel <andi@saerdnaer.de>
2015-03-02 00:34:11 +0100

    fixed indentation by removing whitespace

--- lib/template/places-json.php
+++ lib/template/places-json.php
@@ -1,35 +1,35 @@
 <?php
 	$aFilteredPlaces = array();
 
-		$aFilteredPlaces['licence'] = "Data © OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright";
+	$aFilteredPlaces['licence'] = "Data © OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright";
+	
+	foreach ( $aPlaces AS $aPlace )
+	{
+		$row = array();
+		if ($aPlace['place_id']) $row['place_id'] = $aPlace['place_id'];
+		$row['name'] = $aPlace['placename'];
 		
-		foreach ( $aPlaces AS $aPlace )
+		$sOSMType = ($aPlace['osm_type'] == 'N'?'node':($aPlace['osm_type'] == 'W'?'way':($aPlace['osm_type'] == 'R'?'relation':'')));
+		if ($sOSMType)
 		{
-			$row = array();
-			if ($aPlace['place_id']) $row['place_id'] = $aPlace['place_id'];
-			$row['name'] = $aPlace['placename'];
-			
-			$sOSMType = ($aPlace['osm_type'] == 'N'?'node':($aPlace['osm_type'] == 'W'?'way':($aPlace['osm_type'] == 'R'?'relation':'')));
-			if ($sOSMType)
-			{
-				$aFilteredPlaces['osm_type'] = $sOSMType;
-				$aFilteredPlaces['osm_id'] = $aPlace['osm_id'];
-			}
-			if (isset($aPlace['lat'])) $row['lat'] = $aPlace['lat'];
-			if (isset($aPlace['lon'])) $row['lon'] = $aPlace['lon'];
-
-			$row['place_rank'] = $aPlace['rank_search'];
-
-			$row['category'] = $aPlace['class'];
-			$row['type'] = $aPlace['type'];
-
-			$row['importance'] = $aPlace['importance'];
-
-			$row['addresstype'] = strtolower($aPlace['addresstype']);
-			if ($bShowAddressDetails && $aPlace['aAddress'] && sizeof($aPlace['aAddress'])) $row['address'] = $aPlace['aAddress'];
-			
-			$aFilteredPlaces['results'][] = $row;
+			$aFilteredPlaces['osm_type'] = $sOSMType;
+			$aFilteredPlaces['osm_id'] = $aPlace['osm_id'];
 		}
+		if (isset($aPlace['lat'])) $row['lat'] = $aPlace['lat'];
+		if (isset($aPlace['lon'])) $row['lon'] = $aPlace['lon'];
+
+		$row['place_rank'] = $aPlace['rank_search'];
+
+		$row['category'] = $aPlace['class'];
+		$row['type'] = $aPlace['type'];
+
+		$row['importance'] = $aPlace['importance'];
+
+		$row['addresstype'] = strtolower($aPlace['addresstype']);
+		if ($bShowAddressDetails && $aPlace['aAddress'] && sizeof($aPlace['aAddress'])) $row['address'] = $aPlace['aAddress'];
+		
+		$aFilteredPlaces['results'][] = $row;
+	}
 
 	javascript_renderData($aFilteredPlaces);
 
--- lib/template/places-xml.php
+++ lib/template/places-xml.php
@@ -10,29 +10,29 @@
 	echo " attribution='Data © OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright'";
 	echo ">\n";
 
-		foreach ( $aPlaces AS $aPlace )
-		{
-			echo "<place";
-			if ($aPlace['place_id']) echo ' place_id="'.$aPlace['place_id'].'"';
-			$sOSMType = ($aPlace['osm_type'] == 'N'?'node':($aPlace['osm_type'] == 'W'?'way':($aPlace['osm_type'] == 'R'?'relation':'')));
-			if ($sOSMType) echo ' osm_type="'.$sOSMType.'"'.' osm_id="'.$aPlace['osm_id'].'"';
-			if ($aPlace['ref']) echo ' ref="'.htmlspecialchars($aPlace['ref']).'"';
-			if (isset($aPlace['lat'])) echo ' lat="'.htmlspecialchars($aPlace['lat']).'"';
-			if (isset($aPlace['lon'])) echo ' lon="'.htmlspecialchars($aPlace['lon']).'"';
-			echo ">";
-	
-			if ($bShowAddressDetails) {
-				echo "<addressparts>";
-				foreach($aPlace['aAddress'] as $sKey => $sValue)
-				{
-					$sKey = str_replace(' ','_',$sKey);
-					echo "<$sKey>";
-					echo htmlspecialchars($sValue);
-					echo "</$sKey>";
-				}
-				echo "</addressparts>";
+	foreach ( $aPlaces AS $aPlace )
+	{
+		echo "<place";
+		if ($aPlace['place_id']) echo ' place_id="'.$aPlace['place_id'].'"';
+		$sOSMType = ($aPlace['osm_type'] == 'N'?'node':($aPlace['osm_type'] == 'W'?'way':($aPlace['osm_type'] == 'R'?'relation':'')));
+		if ($sOSMType) echo ' osm_type="'.$sOSMType.'"'.' osm_id="'.$aPlace['osm_id'].'"';
+		if ($aPlace['ref']) echo ' ref="'.htmlspecialchars($aPlace['ref']).'"';
+		if (isset($aPlace['lat'])) echo ' lat="'.htmlspecialchars($aPlace['lat']).'"';
+		if (isset($aPlace['lon'])) echo ' lon="'.htmlspecialchars($aPlace['lon']).'"';
+		echo ">";
+
+		if ($bShowAddressDetails) {
+			echo "<addressparts>";
+			foreach($aPlace['aAddress'] as $sKey => $sValue)
+			{
+				$sKey = str_replace(' ','_',$sKey);
+				echo "<$sKey>";
+				echo htmlspecialchars($sValue);
+				echo "</$sKey>";
 			}
-			echo "</place>";
+			echo "</addressparts>";
 		}
+		echo "</place>";
+	}
 
 	echo "</places>";

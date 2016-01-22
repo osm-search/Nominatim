355d1efe216eca97c2876509032a5c737f040e84
Sarah Hoffmann <lonvia@denofr.de>
2016-01-22 23:01:03 +0100

    add a short introduction text for the web interface

--- lib/template/includes/introduction.php
+++ lib/template/includes/introduction.php
@@ -1,2 +1,7 @@
-<h2>This is Nominatim</h2>
-abc def
+<h2>Welcome to Nominatim</h2>
+
+<p>Nominatim is a search engine for <a href="http://www.openstreetmap.org">OpenStreetMap</a>
+data. This is the debugging interface. You can try search and reverse search queries
+and explore the details of each object saved in the database.</p>
+
+For more information visit the <a href="http://wiki.openstreetmap.org/wiki/Nominatim">Nominatim wiki page</a>.

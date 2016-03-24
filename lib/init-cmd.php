7090604de77517b593707aca13574f460848c9cc
Sarah Hoffmann <lonvia@denofr.de>
2016-03-24 22:06:54 +0100

    reinstate NOMINATIM_SETTINGS variable
    Move sourcing of this script to settings.php so that
    it is loaded before anything else.

--- lib/init-cmd.php
+++ lib/init-cmd.php
@@ -1,9 +1,4 @@
 <?php
-	if (file_exists(getenv('NOMINATIM_SETTINGS')))
-	{
-		require_once(getenv('NOMINATIM_SETTINGS'));
-	}
-
 	require_once('init.php');
 	require_once('cmd.php');
 
--- settings/settings.php
+++ settings/settings.php
@@ -1,6 +1,7 @@
 <?php
 	@define('CONST_BasePath', '@CMAKE_SOURCE_DIR@');
 	@define('CONST_InstallPath', '@CMAKE_BINARY_DIR@');
+	if (file_exists(getenv('NOMINATIM_SETTINGS'))) require_once(getenv('NOMINATIM_SETTINGS'));
 	if (file_exists(CONST_InstallPath.'/settings/local.php')) require_once(CONST_InstallPath.'/settings/local.php');
 	if (isset($_GET['debug']) && $_GET['debug']) @define('CONST_Debug', true);
 

2e08a615655724063ed3e31c766ae666a9df2c57
Sarah Hoffmann <lonvia@denofr.de>
2016-06-08 23:03:11 +0200

    add vagrant script for ubuntu 16 and polish everything

--- Vagrantfile
+++ Vagrantfile
@@ -8,21 +8,28 @@ Vagrant.configure("2") do |config|
   # If true, then any SSH connections made will enable agent forwarding.
   config.ssh.forward_agent = true
 
+  checkout = "yes"
   if ENV['CHECKOUT'] != 'y' then
       config.vm.synced_folder ".", "/home/vagrant/Nominatim"
+      checkout = "no"
   end
 
   config.vm.define "ubuntu" do |sub|
-      sub.vm.box = "ubuntu/trusty64"
-      sub.vm.provision :shell, :path => "vagrant/ubuntu-trusty-provision.sh"
+      sub.vm.box = "bento/ubuntu-16.04"
+      sub.vm.provision :shell do |s|
+        s.path = "vagrant/install-on-ubuntu-16.sh"
+        s.privileged = false
+        s.args = [checkout]
+      end
   end
-  config.vm.define "ubuntu-php7" do |sub|
-      sub.vm.box = "ubuntu/trusty64"
-      sub.vm.provision :shell, :path => "vagrant/ubuntu-trusty-php7-provision.sh"
-  end
+
   config.vm.define "centos" do |sub|
       sub.vm.box = "bento/centos-7.2"
-      sub.vm.provision :shell, :path => "vagrant/install-on-centos-7.sh"
+      sub.vm.provision :shell do |s|
+        s.path = "vagrant/install-on-centos-7.sh"
+        s.privileged = false
+        s.args = [checkout]
+      end
   end
 
   # configure shared package cache if possible
--- vagrant/install-on-ubuntu-16.sh
+++ vagrant/install-on-ubuntu-16.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 #
+# hacks for broken vagrant box      #DOCS:
+sudo rm -f /var/lib/dpkg/lock       #DOCS:
+sudo update-locale LANG=en_US.UTF-8 #DOCS:
+#
 # *Note:* these installation instructions are also available in executable
-#         form for use with vagrant under vagrant/install-on-centos-7.sh.
+#         form for use with vagrant under vagrant/install-on-ubuntu-16.sh.
 #
 # Installing the Required Software
 # ================================
 #
-# These instructions expect that you have a freshly installed CentOS version 7.
+# These instructions expect that you have a freshly installed Ubuntu 16.04.
+#
 # Make sure all packages are are up-to-date by running:
 #
-    sudo yum update -y
 
-# The standard CentOS repositories don't contain all the required packages,
-# you need to enable the EPEL repository as well. To enable it on CentOS,
-# install the epel-release RPM by running:
-
-    sudo yum install -y epel-release
+    sudo apt-get update -qq
+    sudo apt-get upgrade -y
 
 # Now you can install all packages needed for Nominatim:
 
-    sudo yum install -y postgresql-server postgresql-contrib postgresql-devel postgis postgis-utils \
-                        git cmake make gcc gcc-c++ libtool policycoreutils-python \
-                        php-pgsql php php-pear php-pear-DB libpqxx-devel proj-epsg \
-                        bzip2-devel proj-devel geos-devel libxml2-devel boost-devel expat-devel zlib-devel
+    sudo apt-get install -y build-essential cmake g++ libboost-dev libboost-system-dev \
+                            libboost-filesystem-dev libexpat1-dev zlib1g-dev libxml2-dev\
+                            libbz2-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev \
+                            postgresql-server-dev-9.5 postgresql-9.5-postgis-2.2 postgresql-contrib-9.5 \
+                            apache2 php php-pgsql libapache2-mod-php php-pear php-db \
+                            git
 
 # If you want to run the test suite, you need to install the following
 # aditional packages:
 
-    sudo yum install -y python-pip python-Levenshtein python-psycopg2 \
-                        php-phpunit-PHPUnit
-    pip install --user --upgrade pip setuptools lettuce==0.2.18 six==1.9 \
-                                 haversine Shapely pytidylib
+    sudo apt-get install -y python-dev python-pip python-levenshtein python-shapely \
+                            python-psycopg2 tidy python-nose python-tidylib \
+                            phpunit
+
+    pip install --user lettuce==0.2.18 six==1.7 haversine
 
 #
 # System Configuration
 # ====================
 #
-# The following steps are meant to configure a fresh CentOS installation
+# The following steps are meant to configure a fresh Ubuntu installation
 # for use with Nominatim. You may skip some of the steps if you have your
 # OS already configured.
 #
@@ -67,18 +71,11 @@
 # Setting up PostgreSQL
 # ---------------------
 #
-# CentOS does not automatically create a database cluster. Therefore, start
-# with initializing the database, then enable the server to start at boot:
-
-    sudo postgresql-setup initdb
-    sudo systemctl enable postgresql
-
-#
-# Next tune the postgresql configuration, which is located in 
-# `/var/lib/pgsql/data/postgresql.conf`. See section *Postgres Tuning* in
+# Tune the postgresql configuration, which is located in 
+# `/etc/postgresql/9.5/main/postgresql.conf`. See section *Postgres Tuning* in
 # [the installation page](Installation.md) for the parameters to change.
 #
-# Now start the postgresql service after updating this config file.
+# Restart the postgresql service after updating this config file.
 
     sudo systemctl restart postgresql
 
@@ -88,7 +85,7 @@
 #
 
     sudo -u postgres createuser -s $USERNAME
-    sudo -u postgres createuser apache
+    sudo -u postgres createuser www-data
 
 #
 # Setting up the Apache Webserver
@@ -98,7 +95,7 @@
 # configuration. Add a separate nominatim configuration to your webserver:
 
 #DOCS:```
-sudo tee /etc/httpd/conf.d/nominatim.conf << EOFAPACHECONF
+sudo tee /etc/apache2/conf-available/nominatim.conf << EOFAPACHECONF
 <Directory "$USERHOME/build/website"> #DOCS:<Directory "$USERHOME/Nominatim/build/website">
   Options FollowSymLinks MultiViews
   AddType text/html   .php
@@ -109,25 +106,14 @@ Alias /nominatim $USERHOME/build/website  #DOCS:Alias /nominatim $USERHOME/Nomin
 EOFAPACHECONF
 #DOCS:```
 
-sudo sed -i 's:#.*::' /etc/httpd/conf.d/nominatim.conf #DOCS:
+sudo sed -i 's:#.*::' /etc/apache2/conf-available/nominatim.conf #DOCS:
 
 #
-# Then reload apache
+# Then enable the configuration and restart apache
 #
 
-    sudo systemctl restart httpd
-
-#
-# Adding SELinux Security Settings
-# --------------------------------
-#
-# It is a good idea to leave SELinux enabled and enforcing, particularly
-# with a web server accessible from the Internet. At a minimum the
-# following SELinux labeling should be done for Nominatim:
-
-    sudo semanage fcontext -a -t httpd_sys_content_t "$USERHOME/Nominatim/(website|lib|settings)(/.*)?"
-    sudo semanage fcontext -a -t lib_t "$USERHOME/Nominatim/module/nominatim.so"
-    sudo restorecon -R -v $USERHOME/Nominatim
+    sudo a2enconf nominatim
+    sudo systemctl restart apache2
 
 #
 # Installing Nominatim
@@ -157,12 +143,11 @@ fi                                 #DOCS:
     make
 
 # You need to create a minimal configuration file that tells nominatim
-# the name of your webserver user and the URL of the website:
+# where it is located on the webserver:
 
 #DOCS:```
 tee settings/local.php << EOF
 <?php
- @define('CONST_Database_Web_User', 'apache');
  @define('CONST_Website_BaseURL', '/nominatim/');
 EOF
 #DOCS:```

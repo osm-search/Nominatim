b4bed734db9fe28c7c1f0457ca2c37421dec563b
Marc Tobias Metten <mtmail@gmx.net>
2016-06-21 01:03:02 +0200

    testing travis-ci configurationu

---
+++ vagrant/install-on-travis-ci.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+apt-get update -qq
+apt-get upgrade -y --quiet
+
+apt-get install -y --quiet build-essential cmake g++ libboost-dev libboost-system-dev \
+                   libboost-filesystem-dev libexpat1-dev zlib1g-dev libxml2-dev\
+                   libbz2-dev libpq-dev libgeos-dev libgeos++-dev libproj-dev \
+                   postgresql-server-dev-9.5 postgresql-9.5-postgis-2.2 postgresql-contrib-9.5 \
+                   apache2 php php-pgsql libapache2-mod-php php-pear php-db \
+                   git
+
+
+apt-get install -y --quiet python-dev python-pip python-levenshtein python-shapely \
+                   python-psycopg2 tidy python-nose python-tidylib \
+                   phpunit
+
+pip install --quiet --user lettuce==0.2.18 six==1.7 haversine
+
+service postgresql restart
+
+sudo -u postgres createuser -s travis
+sudo -u postgres createuser www-data
+
+sudo tee /etc/apache2/conf-available/nominatim.conf << EOFAPACHECONF
+<Directory "$USERHOME/build/website">
+  Options FollowSymLinks MultiViews
+  AddType text/html   .php
+  Require all granted
+</Directory>
+
+Alias /nominatim $USERHOME/build/website
+EOFAPACHECONF
+
+sudo sed -i 's:#.*::' /etc/apache2/conf-available/nominatim.conf
+
+sudo a2enconf nominatim
+sudo service apache2 restart
+
+cd $TRAVIS_BUILD_DIR
+mkdir build
+cd build
+cmake $TRAVIS_BUILD_DIR # /Nominatim
+make
+
+
+tee settings/local.php << EOF
+<?php
+ @define('CONST_Website_BaseURL', '/nominatim/');
+EOF

3f421b42056a4da2f8bc469abecc6f4a510feabf
Marc Tobias Metten <mtmail@gmx.net>
2016-07-31 17:17:17 +0200

    php linting: remove first ident in <?php blocks

--- website/lookup.php
+++ website/lookup.php
@@ -1,73 +1,74 @@
 <?php
-	@define('CONST_ConnectionBucket_PageType', 'Reverse');
 
-	require_once(dirname(dirname(__FILE__)).'/settings/settings.php');
-	require_once(CONST_BasePath.'/lib/init-website.php');
-	require_once(CONST_BasePath.'/lib/log.php');
-	require_once(CONST_BasePath.'/lib/PlaceLookup.php');
-	require_once(CONST_BasePath.'/lib/output.php');
+@define('CONST_ConnectionBucket_PageType', 'Reverse');
 
-	$oDB =& getDB();
-	ini_set('memory_limit', '200M');
+require_once(dirname(dirname(__FILE__)).'/settings/settings.php');
+require_once(CONST_BasePath.'/lib/init-website.php');
+require_once(CONST_BasePath.'/lib/log.php');
+require_once(CONST_BasePath.'/lib/PlaceLookup.php');
+require_once(CONST_BasePath.'/lib/output.php');
 
-	// Format for output
-	$sOutputFormat = getParamSet('format', array('xml', 'json'), 'xml');
+$oDB =& getDB();
+ini_set('memory_limit', '200M');
 
-	// Preferred language
-	$aLangPrefOrder = getPreferredLanguages();
+// Format for output
+$sOutputFormat = getParamSet('format', array('xml', 'json'), 'xml');
 
-	$hLog = logStart($oDB, 'place', $_SERVER['QUERY_STRING'], $aLangPrefOrder);
+// Preferred language
+$aLangPrefOrder = getPreferredLanguages();
 
-	$aSearchResults = array();
-	$aCleanedQueryParts = array();
+$hLog = logStart($oDB, 'place', $_SERVER['QUERY_STRING'], $aLangPrefOrder);
 
-	$oPlaceLookup = new PlaceLookup($oDB);
-	$oPlaceLookup->setLanguagePreference($aLangPrefOrder);
-	$oPlaceLookup->setIncludeAddressDetails(getParamBool('addressdetails', true));
-	$oPlaceLookup->setIncludeExtraTags(getParamBool('extratags', false));
-	$oPlaceLookup->setIncludeNameDetails(getParamBool('namedetails', false));
+$aSearchResults = array();
+$aCleanedQueryParts = array();
 
-	$aOsmIds = explode(',', getParamString('osm_ids', ''));
+$oPlaceLookup = new PlaceLookup($oDB);
+$oPlaceLookup->setLanguagePreference($aLangPrefOrder);
+$oPlaceLookup->setIncludeAddressDetails(getParamBool('addressdetails', true));
+$oPlaceLookup->setIncludeExtraTags(getParamBool('extratags', false));
+$oPlaceLookup->setIncludeNameDetails(getParamBool('namedetails', false));
 
-	if (count($aOsmIds) > CONST_Places_Max_ID_count)
+$aOsmIds = explode(',', getParamString('osm_ids', ''));
+
+if (count($aOsmIds) > CONST_Places_Max_ID_count)
+{
+	userError('Bulk User: Only ' . CONST_Places_Max_ID_count . " ids are allowed in one request.");
+}
+
+foreach ($aOsmIds AS $sItem)
+{
+	// Skip empty sItem
+	if (empty($sItem)) continue;
+	
+	$sType = $sItem[0];
+	$iId = (int) substr($sItem, 1);
+	if ( $iId > 0 && ($sType == 'N' || $sType == 'W' || $sType == 'R') )
 	{
-		userError('Bulk User: Only ' . CONST_Places_Max_ID_count . " ids are allowed in one request.");
-	}
-
-	foreach ($aOsmIds AS $sItem)
-	{
-		// Skip empty sItem
-		if (empty($sItem)) continue;
-		
-		$sType = $sItem[0];
-		$iId = (int) substr($sItem, 1);
-		if ( $iId > 0 && ($sType == 'N' || $sType == 'W' || $sType == 'R') )
-		{
-			$aCleanedQueryParts[] = $sType . $iId;
-			$oPlace = $oPlaceLookup->lookupOSMID($sType, $iId);
-			if ($oPlace){
-				// we want to use the search-* output templates, so we need to fill
-				// $aSearchResults and slightly change the (reverse search) oPlace
-				// key names
-				$oResult = $oPlace;
-				unset($oResult['aAddress']);
-				if (isset($oPlace['aAddress'])) $oResult['address'] = $oPlace['aAddress'];
-				unset($oResult['langaddress']);
-				$oResult['name'] = $oPlace['langaddress'];
-				$aSearchResults[] = $oResult;
-			}
+		$aCleanedQueryParts[] = $sType . $iId;
+		$oPlace = $oPlaceLookup->lookupOSMID($sType, $iId);
+		if ($oPlace){
+			// we want to use the search-* output templates, so we need to fill
+			// $aSearchResults and slightly change the (reverse search) oPlace
+			// key names
+			$oResult = $oPlace;
+			unset($oResult['aAddress']);
+			if (isset($oPlace['aAddress'])) $oResult['address'] = $oPlace['aAddress'];
+			unset($oResult['langaddress']);
+			$oResult['name'] = $oPlace['langaddress'];
+			$aSearchResults[] = $oResult;
 		}
 	}
+}
 
 
-	if (CONST_Debug) exit;
+if (CONST_Debug) exit;
 
-	$sXmlRootTag = 'lookupresults';
-	$sQuery = join(',',$aCleanedQueryParts);
-	// we initialize these to avoid warnings in our logfile
-	$sViewBox = '';
-	$bShowPolygons = '';
-	$aExcludePlaceIDs = [];
-	$sMoreURL = '';
+$sXmlRootTag = 'lookupresults';
+$sQuery = join(',',$aCleanedQueryParts);
+// we initialize these to avoid warnings in our logfile
+$sViewBox = '';
+$bShowPolygons = '';
+$aExcludePlaceIDs = [];
+$sMoreURL = '';
 
-	include(CONST_BasePath.'/lib/template/search-'.$sOutputFormat.'.php');
+include(CONST_BasePath.'/lib/template/search-'.$sOutputFormat.'.php');

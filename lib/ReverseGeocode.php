e3014f6bd5ef19f65f85875cf8719e7e45000c6d
Janderson Martins <janderson@locadados.com>
2016-11-23 17:01:48 -0200

    pull the increment of search diameter to bottom while structure

--- lib/ReverseGeocode.php
+++ lib/ReverseGeocode.php
@@ -63,8 +63,6 @@ class ReverseGeocode
         $bPlaceIsTiger = false;
         $bPlaceIsLine = false;
         while (!$iPlaceID && $fSearchDiam < $fMaxAreaDistance) {
-            $fSearchDiam = $fSearchDiam * 2;
-
             // If we have to expand the search area by a large amount then we need a larger feature
             // then there is a limit to how small the feature should be
             if ($fSearchDiam > 2 && $iMaxRank > 4) $iMaxRank = 4;
@@ -94,6 +92,8 @@ class ReverseGeocode
             $iPlaceID = $aPlace['place_id'];
             $iParentPlaceID = $aPlace['parent_place_id'];
             $bIsInUnitedStates = ($aPlace['calculated_country_code'] == 'us');
+            
+            $fSearchDiam = $fSearchDiam * 2;
         }
         // if a street or house was found, look in interpolation lines table
         if ($bDoInterpolation && $this->iMaxRank >= 28 && $aPlace && $aPlace['rank_search'] >= 26) {

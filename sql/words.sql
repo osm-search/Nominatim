166737fb76268d58142e0519b3124ef6adcdef19
Sarah Hoffmann <lonvia@denofr.de>
2017-04-18 22:11:09 +0200

    also use address column for word count computation

--- sql/words.sql
+++ sql/words.sql
@@ -4,8 +4,8 @@ CREATE TABLE word_frequencies AS
     WHERE v is not null
  GROUP BY id);
 
-select count(make_keywords(v)) from (select distinct postcode as v from place) as w where v is not null;
-select count(getorcreate_housenumber_id(make_standard_name(v))) from (select distinct housenumber as v from place where housenumber is not null) as w;
+select count(make_keywords(v)) from (select distinct address->'postcode' as v from place where address ? 'postcode') as w where v is not null;
+select count(getorcreate_housenumber_id(make_standard_name(v))) from (select distinct address->'housenumber' as v from place where address ? 'housenumber') as w;
 
 -- copy the word frequencies
 update word set search_name_count = count from word_frequencies wf where wf.id = word.word_id;

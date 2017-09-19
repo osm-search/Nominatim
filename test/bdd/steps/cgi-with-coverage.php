af74c037f4fa28858fb36086b41b87e03921a049
Sarah Hoffmann <lonvia@denofr.de>
2017-09-19 22:42:09 +0200

    enable coverage also for tests with HTTP errors

--- test/bdd/steps/cgi-with-coverage.php
+++ test/bdd/steps/cgi-with-coverage.php
@@ -1,13 +1,20 @@
 <?php
 require_once 'SebastianBergmann/CodeCoverage/autoload.php';
+
+function coverage_shutdown($oCoverage)
+{
+    $oCoverage->stop();
+    $writer = new \SebastianBergmann\CodeCoverage\Report\PHP;
+    $writer->process($oCoverage, $_SERVER['PHP_CODE_COVERAGE_FILE']);
+}
+
 $covfilter = new SebastianBergmann\CodeCoverage\Filter();
 $covfilter->addDirectoryToWhitelist($_SERVER['COV_PHP_DIR']);
 $coverage = new SebastianBergmann\CodeCoverage\CodeCoverage(null, $covfilter);
 $coverage->start($_SERVER['COV_TEST_NAME']);
 
+register_shutdown_function('coverage_shutdown', $coverage);
+
 include $_SERVER['COV_SCRIPT_FILENAME'];
 
 
-$coverage->stop();
-$writer = new \SebastianBergmann\CodeCoverage\Report\PHP;
-$writer->process($coverage, $_SERVER['PHP_CODE_COVERAGE_FILE']);

e523b34db97d1a1f9ab3b0b07cf12f114a5166a1
Sarah Hoffmann <lonvia@denofr.de>
2017-10-28 18:24:01 +0200

    precomputed postcodes must use dedicated import function

--- sql/words.sql
+++ sql/words.sql
@@ -4,7 +4,7 @@ CREATE TABLE word_frequencies AS
     WHERE v is not null
  GROUP BY id);
 
-select count(make_keywords(v)) from (select distinct address->'postcode' as v from place where address ? 'postcode') as w where v is not null;
+select count(getorcreate_postcode_id(v)) from (select distinct address->'postcode' as v from place where address ? 'postcode') as w where v is not null;
 select count(getorcreate_housenumber_id(make_standard_name(v))) from (select distinct address->'housenumber' as v from place where address ? 'housenumber') as w;
 
 -- copy the word frequencies

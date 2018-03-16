4ac1bf2d472a41726e75c49f79bfb6321e09c63c
Sarah Hoffmann <lonvia@denofr.de>
2018-03-16 23:02:07 +0100

    clean up byte order detection
    Check for existence of the expected functions and macros
    and error out if nothing appropriate can be found.

--- nominatim/postgresql.h
+++ nominatim/postgresql.h
@@ -7,20 +7,32 @@
 #define PG_OID_INT8			20
 #define PG_OID_INT4			23
 
-#if defined(HAVE_BYTESWAP_H)
+#if HAVE_BYTESWAP
 #include <byteswap.h>
-#elif defined(HAVE_SYS_ENDIAN_H)
+#define PG_BSWAP32(x) bswap_32(x)
+#define PG_BSWAP64(x) bswap_64(x)
+#elif HAVE_SYS_ENDIAN
 #include <sys/endian.h>
+#define PG_BSWAP32(x) bswap32(x)
+#define PG_BSWAP64(x) bswap64(x)
+#else
+#error "No appropriate byteswap found for your system."
 #endif
 
-#if __BYTE_ORDER == __BIG_ENDIAN
-#define PGint16(x)	(x)
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
 #define PGint32(x)	(x)
 #define PGint64(x)	(x)
+#elif defined(__BYTE_ORDER__) &&  (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define PGint32(x)	PG_BSWAP32(x)
+#define PGint64(x)	PG_BSWAP64(x)
+#elif defined(_BYTE_ORDER) && (_BYTE_ORDER == _BIG_ENDIAN)
+#define PGint32(x)	(x)
+#define PGint64(x)	(x)
+#elif defined(_BYTE_ORDER) &&  (_BYTE_ORDER == _LITTLE_ENDIAN)
+#define PGint32(x)	PG_BSWAP32(x)
+#define PGint64(x)	PG_BSWAP64(x)
 #else
-#define PGint16(x)	__bswap_16 (x)
-#define PGint32(x)	__bswap_32 (x)
-#define PGint64(x)	__bswap_64 (x)
+#error "Cannot determine byte order."
 #endif
 
 const char *build_conninfo(const char *db, const char *username, const char *password, const char *host, const char *port);

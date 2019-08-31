c863a31fd93ee71d84fe4a3bf82448459343a5ea
tchaddad <tchaddad@gmail.com>
2019-08-31 08:40:29 +0200

    update import_wikidata.sh

--- data-sources/wikipedia-wikidata/import_wikidata.sh
+++ data-sources/wikipedia-wikidata/import_wikidata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 psqlcmd() {
-     psql wikiprocessingdb
+     psql wikiprocessing_db
 }
 
 mysql2pgsqlcmd() {
@@ -14,7 +14,7 @@ mysql2pgsqlcmd() {
 language=( "ar" "bg" "ca" "cs" "da" "de" "en" "es" "eo" "eu" "fa" "fr" "ko" "hi" "hr" "id" "it" "he" "lt" "hu" "ms" "nl" "ja" "no" "pl" "pt" "kk" "ro" "ru" "sk" "sl" "sr" "fi" "sv" "tr" "uk" "vi" "vo" "war" "zh" )
 
 
-# get wikidata tables
+# get a few wikidata dump tables
 
 wget https://dumps.wikimedia.org/wikidatawiki/latest/wikidatawiki-latest-geo_tags.sql.gz
 wget https://dumps.wikimedia.org/wikidatawiki/latest/wikidatawiki-latest-page.sql.gz
@@ -28,18 +28,11 @@ gzip -dc wikidatawiki-latest-page.sql.gz | mysql2pgsqlcmd | psqlcmd
 gzip -dc wikidatawiki-latest-wb_items_per_site.sql.gz | mysql2pgsqlcmd | psqlcmd
 
 
-# create derrived tables
-
-echo "CREATE TABLE geo_earth_primary AS SELECT gt_page_id, gt_lat, gt_lon FROM geo_tags WHERE gt_globe = 'earth' AND gt_primary = 1 AND NOT( gt_lat < -90 OR gt_lat > 90 OR gt_lon < -180 OR gt_lon > 180 OR gt_lat=0 OR gt_lon=0) ;" | psqlcmd
-echo "CREATE TABLE geo_earth_wikidata AS SELECT DISTINCT geo_earth_primary.gt_page_id, geo_earth_primary.gt_lat, geo_earth_primary.gt_lon, page.page_title, page.page_namespace FROM geo_earth_primary LEFT OUTER JOIN page ON (geo_earth_primary.gt_page_id = page.page_id) ORDER BY geo_earth_primary.gt_page_id;" | psqlcmd
-echo "CREATE TABLE geo_earth_wikidata_pages (gt_page_id integer, gt_lat numeric(11,8), gt_lon numeric(11,8), language text, page_title text, instance_of text, ips_site_page text);" | psqlcmd
-
-
 # get wikidata places from wikidata query API
 
 while read F  ; do
-    wget "https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=SELECT ?item?lat?lon WHERE{?item wdt:P31*/wdt:P279*wd:$F;wdt:P625?pt.?item p:P625?loc.?loc psv:P625?cnode.?cnode wikibase:geoLatitude?lat.?cnode wikibase:geoLongitude?lon.}" -O $F.json
-    jq -r '.results | .[] | .[] | [.item.value,.lat.value, .lon.value] | @csv ' $F.json >> $F.txt
+    wget "https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=SELECT ?item WHERE{?item wdt:P31*/wdt:P279*wd:$F;}" -O $F.json
+    jq -r '.results | .[] | .[] | [.item.value] | @csv' $F.json >> $F.txt
     awk -v qid=$F '{print $0 ","qid}' $F.txt | sed -e 's!"http://www.wikidata.org/entity/!!' | sed 's/"//g' >> $F.csv
     cat $F.csv >> wikidata_place_dump.csv
     rm $F.json $F.txt $F.csv
@@ -48,41 +41,54 @@ done < wikidata_place_types.txt
 
 # import wikidata places
 
-echo "CREATE TABLE wikidata_place_dump (item text,  lat numeric(11,8), lon numeric(11,8), instance_of text, ont_level integer);"  | psqlcmd
-echo "COPY wikidata_place_dump (item, lat, lon, instance_of) FROM  '/srv/nominatim/Nominatim/data-sources/wikipedia-wikidata/wikidata_place_dump.csv' DELIMITER ',' CSV;"  | psqlcmd
-echo "CREATE TABLE wikidata_places AS SELECT DISTINCT item, instance_of, lon, lat, ont_level FROM wikidata_place_types;" | psqlcmd
+echo "CREATE TABLE wikidata_place_dump (item text, instance_of text);"  | psqlcmd
+echo "COPY wikidata_place_dump (item, instance_of) FROM  '/srv/nominatim/Nominatim/data-sources/wikipedia-wikidata/wikidata_place_dump.csv' DELIMITER ',' CSV;"  | psqlcmd
+
 echo "CREATE TABLE wikidata_place_type_levels (place_type text, level integer);" | psqlcmd
-echo "COPY wikidata_place_type_levels(place_type, level) FROM '/srv/nominatim/Nominatim/data-sources/wikipedia-wikidata/wikidata_place_type_levels.csv' DELIMITER ',' CSV HEADER;" | psqlcmd
-echo "UPDATE wikidata_places SET ont_level = level FROM wikidata_place_type_levels WHERE instance_of = place_type;" | psqlcmd
+echo "COPY wikidata_place_type_levels (place_type, level) FROM '/srv/nominatim/Nominatim/data-sources/wikipedia-wikidata/wikidata_place_type_levels.csv' DELIMITER ',' CSV HEADER;" | psqlcmd
 
 
-# create wikidata language pages with wikipedia links
+# create derived tables
+
+echo "CREATE TABLE geo_earth_primary AS SELECT gt_page_id, gt_lat, gt_lon FROM geo_tags WHERE gt_globe = 'earth' AND gt_primary = 1 AND NOT( gt_lat < -90 OR gt_lat > 90 OR gt_lon < -180 OR gt_lon > 180 OR gt_lat=0 OR gt_lon=0) ;" | psqlcmd
+echo "CREATE TABLE geo_earth_wikidata AS SELECT DISTINCT geo_earth_primary.gt_page_id, geo_earth_primary.gt_lat, geo_earth_primary.gt_lon, page.page_title, page.page_namespace FROM geo_earth_primary LEFT OUTER JOIN page ON (geo_earth_primary.gt_page_id = page.page_id) ORDER BY geo_earth_primary.gt_page_id;" | psqlcmd
+
+echo "ALTER TABLE wikidata_place_dump ADD COLUMN ont_level integer, ADD COLUMN lat numeric(11,8), ADD COLUMN lon numeric(11,8);" | psqlcmd
+echo "UPDATE wikidata_place_dump SET ont_level = wikidata_place_type_levels.level FROM wikidata_place_type_levels WHERE wikidata_place_dump.instance_of = wikidata_place_type_levels.place_type;" | psqlcmd
+
+echo "CREATE TABLE wikidata_places AS SELECT DISTINCT ON (item) item, instance_of, MAX(ont_level) AS ont_level, lat, lon FROM wikidata_place_dump GROUP BY item, instance_of, ont_level, lat, lon ORDER BY item;" | psqlcmd
+echo "UPDATE wikidata_places SET lat = geo_earth_wikidata.gt_lat, lon = geo_earth_wikidata.gt_lon FROM geo_earth_wikidata WHERE wikidata_places.item = geo_earth_wikidata.page_title" | psqlcmd
+
+
+# process language pages 
+
+echo "CREATE TABLE wikidata_pages (item text, instance_of text, lat numeric(11,8), lon numeric(11,8), ips_site_page text, language text );" | psqlcmd
 
 for i in "${language[@]}"
 do
-   echo "create table geo_earth_wikidata_${i}_pages as select geo_earth_wikidata_places.gt_page_id, geo_earth_wikidata_places.gt_lat, geo_earth_wikidata_places.gt_lon, geo_earth_wikidata_places.page_title, geo_earth_wikidata_places.instance_of, wb_items_per_site.ips_site_page FROM geo_earth_wikidata_places LEFT JOIN wb_items_per_site ON (CAST (( LTRIM(geo_earth_wikidata_places.page_title, 'Q')) AS INTEGER) = wb_items_per_site.ips_item_id) WHERE ips_site_id = '${i}wiki' order by geo_earth_wikidata_places.gt_page_id;" | psqlcmd
-   echo "insert into geo_earth_wikidata_pages select gt_page_id, gt_lat, gt_lon, '${i}', page_title, instance_of, ips_site_page from geo_earth_wikidata_${i}_pages;" | psqlcmd
+   echo "CREATE TABLE wikidata_${i}_pages as select wikidata_places.item, wikidata_places.instance_of, wikidata_places.lat, wikidata_places.lon, wb_items_per_site.ips_site_page FROM wikidata_places LEFT JOIN wb_items_per_site ON (CAST (( LTRIM(wikidata_places.item, 'Q')) AS INTEGER) = wb_items_per_site.ips_item_id) WHERE ips_site_id = '${i}wiki' AND LEFT(wikidata_places.item,1) = 'Q' order by wikidata_places.item;" | psqlcmd
+   echo "ALTER TABLE wikidata_${i}_pages ADD COLUMN language text;" | psqlcmd
+   echo "UPDATE wikidata_${i}_pages SET language = '${i}';" | psqlcmd
+   echo "INSERT INTO wikidata_pages SELECT item, instance_of, lat, lon, ips_site_page, language FROM wikidata_${i}_pages;" | psqlcmd
 done
 
-echo "ALTER TABLE geo_earth_wikidata_pages ADD COLUMN wp_page_title text;" | psqlcmd
-echo "UPDATE geo_earth_wikidata_pages SET wp_page_title = REPLACE(ips_site_page, ' ', '_');" | psqlcmd
+echo "ALTER TABLE wikidata_pages ADD COLUMN wp_page_title text;" | psqlcmd
+echo "UPDATE wikidata_pages SET wp_page_title = REPLACE(ips_site_page, ' ', '_');" | psqlcmd
+echo "ALTER TABLE wikidata_pages DROP COLUMN ips_site_page;" | psqlcmd
 
 
 # add wikidata to wikipedia_article table
 
-echo "UPDATE wikipedia_article SET lat = geo_earth_wikidata_pages.gt_lat, lon = geo_earth_wikidata_pages.gt_lon, wd_page_title = geo_earth_wikidata_pages.page_title, instance_of = geo_earth_wikidata_pages.instance_of FROM geo_earth_wikidata_pages WHERE wikipedia_article.language = geo_earth_wikidata_pages.language AND wikipedia_article.title  = geo_earth_wikidata_pages.wp_page_title AND geo_earth_wikidata_pages.gt_lat IS NOT NULL;" | psqlcmd
-
-
-
-
+echo "UPDATE wikipedia_article SET lat = wikidata_pages.lat, lon = wikidata_pages.lon, wd_page_title = wikidata_pages.item, instance_of = wikidata_pages.instance_of FROM wikidata_pages WHERE wikipedia_article.language = wikidata_pages.language AND wikipedia_article.title  = wikidata_pages.wp_page_title;" | psqlcmd
+echo "ALTER TABLE wikipedia_article RENAME TO wikipedia_article_full;" | psqlcmd
+echo "ALTER TABLE wikipedia_article_slim RENAME TO wikipedia_article;" | psqlcmd
 
 
 # clean up intermediate tables
 
 echo "DROP TABLE wikidata_place_dump;" | psqlcmd
-echo "DROP TABLE wikidata_place_type_levels;" | psqlcmd
 echo "DROP TABLE geo_earth_primary;" | psqlcmd
 for i in "${language[@]}"
 do
-    echo "DROP TABLE geo_earth_wikidata_${i}_pages;" | psqlcmd
+    echo "DROP TABLE wikidata_${i}_pages;" | psqlcmd
 done

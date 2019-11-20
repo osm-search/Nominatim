000fe3ddffc591d28179daf166c63cf90a7d0472
Sarah Hoffmann <lonvia@denofr.de>
2019-11-20 11:49:25 +0100

    remove reference to nominatim.xml OpenSearch description
    The file was meant for the openstreetmap instance only.
    
    Fixes #1554.

--- lib/template/includes/html-header.php
+++ lib/template/includes/html-header.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <base href="<?php echo CONST_Website_BaseURL;?>" />
-    <link href="nominatim.xml" rel="search" title="Nominatim Search" type="application/opensearchdescription+xml" />
     <link href="css/leaflet.css" rel="stylesheet" />
     <link href="css/Control.Minimap.min.css" rel="stylesheet" />
     <link href="css/bootstrap-theme.min.css" rel="stylesheet" />

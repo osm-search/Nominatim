c537ea18a4c158359f16af7709f504d6e9fdca4e
Sarah Hoffmann <lonvia@denofr.de>
2020-01-05 16:32:12 +0100

    move functions for interpolation table in own file

--- lib/setup/SetupClass.php
+++ lib/setup/SetupClass.php
@@ -653,6 +653,7 @@ class SetupFunctions
         $sTemplate .= file_get_contents($sBasePath.'normalization.sql');
         $sTemplate .= file_get_contents($sBasePath.'importance.sql');
         $sTemplate .= file_get_contents($sBasePath.'address_lookup.sql');
+        $sTemplate .= file_get_contents($sBasePath.'interpolation.sql');
         $sTemplate = str_replace('{modulepath}', $this->sModulePath, $sTemplate);
         if ($this->bEnableDiffUpdates) {
             $sTemplate = str_replace('RETURN NEW; -- %DIFFUPDATES%', '--', $sTemplate);

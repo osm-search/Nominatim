3b6c2c9155ada3706c9095efc51fcd4213141b4f
Sarah Hoffmann <lonvia@denofr.de>
2020-01-16 21:43:26 +0100

    getNearestNamed*Feature functions better return values

--- sql/functions/utils.sql
+++ sql/functions/utils.sql
@@ -248,7 +248,6 @@ LANGUAGE plpgsql STABLE;
 -- \param centroid   Location of the address.
 --
 -- \return Place ID of the parent if one was found, NULL otherwise.
---         The returned parent is always a street (rank 26/27 and a way).
 CREATE OR REPLACE FUNCTION find_parent_for_address(street TEXT, place TEXT,
                                                    partition SMALLINT,
                                                    centroid GEOMETRY)
@@ -263,9 +262,7 @@ BEGIN
     -- Note that addr:street links can only be indexed, once the street itself is indexed
     word_ids := word_ids_from_name(street);
     IF word_ids is not null THEN
-      SELECT place_id
-        FROM getNearestNamedRoadFeature(partition, centroid, word_ids)
-        INTO parent_place_id;
+      parent_place_id := getNearestNamedRoadFeature(partition, centroid, word_ids);
       IF parent_place_id is not null THEN
         --DEBUG: RAISE WARNING 'Get parent form addr:street: %', parent.place_id;
         RETURN parent_place_id;
@@ -277,9 +274,7 @@ BEGIN
   IF place is not null THEN
     word_ids := word_ids_from_name(place);
     IF word_ids is not null THEN
-      SELECT place_id
-        FROM getNearestNamedPlaceFeature(partition, centroid, word_ids)
-        INTO parent_place_id;
+      parent_place_id := getNearestNamedPlaceFeature(partition, centroid, word_ids);
       IF parent_place_id is not null THEN
         --DEBUG: RAISE WARNING 'Get parent form addr:place: %', parent.place_id;
         RETURN parent_place_id;

96c6e030a9344cfe5963dba1bd3720f0127e6e8c
Gregory <glp@xplornet.com>
2020-03-25 10:32:52 -0400

    Add selector
    Required some CSS changes to be pretty

--- lib/template/search-html.php
+++ lib/template/search-html.php
@@ -10,10 +10,60 @@
 
     <?php include(CONST_BasePath.'/lib/template/includes/html-top-navigation.php'); ?>
 
-    <form class="form-inline" role="search" accept-charset="UTF-8" action="<?php echo CONST_Website_BaseURL; ?>search.php">
-        <div class="form-group">
-            <input id="q" name="q" type="text" class="form-control input-sm" placeholder="Search" value="<?php echo htmlspecialchars($sQuery); ?>" >
+    <div class="top-bar" id="structured-query-selector">
+        <div class="search-type-link">
+            <a id="switch-to-reverse" href="<?php echo CONST_Website_BaseURL; ?>reverse.php?format=html">reverse search</a>
         </div>
+        <?php
+        if (empty($aMoreParams['q']) and empty($aMoreParams['street']) and empty($aMoreParams['city']) and empty($aMoreParams['county']) and empty($aMoreParams['state']) and empty($aMoreParams['country']) and empty($aMoreParams['postalcode'])) {
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="simple" value="simple" checked="checked">';
+            echo '<label for="simple">simple</label></div>';
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="structured" value="structured">';
+            echo '<label for="structured">structured</label></div>';
+        }
+        elseif (!empty($aMoreParams['q'])) {
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="simple" value="simple" checked="checked">';
+            echo '<label for="simple">simple</label></div>';
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="structured" value="structured">';
+            echo '<label for="structured">structured</label></div>';
+        }
+        else {
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="simple" value="simple">';
+            echo '<label for="simple">simple</label></div>';
+            echo '<div class="radio-inline"><input type="radio" name="query-selector" id="structured" value="structured" checked="checked">';
+            echo '<label for="structured">structured</label></div>';
+        }
+        ?>
+
+    <form role="search" accept-charset="UTF-8" action="<?php echo CONST_Website_BaseURL; ?>search.php">
+        <div class="form-group-simple"
+        <?php
+        if (empty($aMoreParams['q']) and empty($aMoreParams['street']) and empty($aMoreParams['city']) and empty($aMoreParams['county']) and empty($aMoreParams['state']) and empty($aMoreParams['country']) and empty($aMoreParams['postalcode'])) {
+            echo "style='display:block;'";
+        }
+        elseif (empty($aMoreParams['q'])) {
+            echo "style='display:none;'";
+        }
+        ?>>
+            <input id="q" name="q" type="text" class="form-control input-sm" placeholder="Search" value="<?php echo $aMoreParams['q']; ?>" >
+        </div>
+        <div class="form-group-structured"
+        <?php
+        if (empty($aMoreParams['street']) and empty($aMoreParams['city']) and empty($aMoreParams['county']) and empty($aMoreParams['state']) and empty($aMoreParams['country']) and empty($aMoreParams['postalcode'])) {
+            echo "style='display:none;'";
+        }
+        else {
+            echo "style='display:block;'";
+        }
+        ?>>
+<div class="form-inline">
+            <input id="street" name="street" type="text" class="form-control input-sm" placeholder="House number/Street" value="<?php echo $aMoreParams['street']; ?>" >
+            <input id="city" name="city" type="text" class="form-control input-sm" placeholder="City" value="<?php echo $aMoreParams['city']; ?>" >
+            <input id="county" name="county" type="text" class="form-control input-sm" placeholder="County" value="<?php echo $aMoreParams['county']; ?>" >
+            <input id="state" name="state" type="text" class="form-control input-sm" placeholder="State" value="<?php echo $aMoreParams['state']; ?>" >
+            <input id="country" name="country" type="text" class="form-control input-sm" placeholder="Country" value="<?php echo $aMoreParams['country']; ?>" >
+            <input id="postalcode" name="postalcode" type="text" class="form-control input-sm" placeholder="Postal Code" value="<?php echo $aMoreParams['postalcode']; ?>" >
+        </div></div>
         <div class="form-group search-button-group">
             <button type="submit" class="btn btn-primary btn-sm">Search</button>
             <?php if (CONST_Search_AreaPolygons) { ?>
@@ -25,11 +75,8 @@
                 <label for="use_viewbox">apply viewbox</label>
             </div>
         </div>
-        <div class="search-type-link">
-            <a id="switch-to-reverse" href="<?php echo CONST_Website_BaseURL; ?>reverse.php?format=html">reverse search</a>
-        </div>
     </form>
-
+</div>
 
     <div id="content">
 
@@ -86,10 +133,6 @@
         </div>
 
     </div> <!-- /content -->
-
-
-
-
 
 
 

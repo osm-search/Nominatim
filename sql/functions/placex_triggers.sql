fae02fab00779b0f4c9aa3d6d541ba2c7eba165f
Sarah Hoffmann <lonvia@denofr.de>
2020-09-01 17:59:26 +0200

    address rank adjustment for addressable boundaries only
    Only administrative boundaries with an address rank need
    to be adjusted. Otherwise just handle them like any other
    object.

--- sql/functions/placex_triggers.sql
+++ sql/functions/placex_triggers.sql
@@ -596,7 +596,9 @@ BEGIN
                             (NEW.extratags->'capital') = 'yes',
                             NEW.address->'postcode');
   -- We must always increase the address level relative to the admin boundary.
-  IF NEW.class = 'boundary' and NEW.type = 'administrative' and NEW.osm_type = 'R' THEN
+  IF NEW.class = 'boundary' and NEW.type = 'administrative'
+     and NEW.osm_type = 'R' and NEW.rank_address > 0
+  THEN
     parent_address_level := get_parent_address_level(NEW.centroid, NEW.admin_level);
     IF parent_address_level >= NEW.rank_address THEN
       IF parent_address_level >= 24 THEN

7ac22e92279fc77db316623c76cfcf6cbb22e070
marc tobias <mtmail@gmx.net>
2020-09-14 00:45:22 +0200

    starting PHP 5.4 get_magic_quotes_gpc() returns false, no need to check

--- lib/init.php
+++ lib/init.php
@@ -2,8 +2,3 @@
 
 require_once(CONST_BasePath.'/lib/lib.php');
 require_once(CONST_BasePath.'/lib/DB.php');
-
-if (get_magic_quotes_gpc()) {
-    echo "Please disable magic quotes in your php.ini configuration\n";
-    exit;
-}

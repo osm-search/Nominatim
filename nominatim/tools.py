612fd5061275557617dc0726ea08e4c5b82b3474
Sarah Hoffmann <lonvia@denofr.de>
2021-01-12 10:17:28 +0100

    add skeleton for new Nominatim executables

---
+++ nominatim/tools.py
@@ -0,0 +1,14 @@
+"""
+Provides the fronting for the Nominatim tools, command line and environment
+parsing.
+"""
+
+def setup(**kwargs):
+    print("Functions for creating a Nominatim database and importing data.")
+
+def update(**kwargs):
+    print("Functions for updating a Nominatim database.")
+
+def admin(**kwargs):
+    print("Functions for maintaining a Nomiantim database.")
+

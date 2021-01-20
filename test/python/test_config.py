bfa6580ad59610d5286e35c7ca62edc03d5020f7
Sarah Hoffmann <lonvia@denofr.de>
2021-01-20 09:03:09 +0100

    use pytest mocking functions for manipulating os.environ

--- test/python/test_config.py
+++ test/python/test_config.py
@@ -3,7 +3,6 @@ Test for loading dotenv configuration.
 """
 from pathlib import Path
 import tempfile
-import os
 
 import pytest
 
@@ -25,51 +24,48 @@ def test_prefer_project_setting_over_default():
 
         assert config.DATABASE_WEBUSER == 'apache'
 
-def test_prefer_os_environ_over_project_setting():
+def test_prefer_os_environ_over_project_setting(monkeypatch):
     with tempfile.TemporaryDirectory() as project_dir:
         with open(project_dir + '/.env', 'w') as envfile:
             envfile.write('NOMINATIM_DATABASE_WEBUSER=apache\n')
 
-        os.environ['NOMINATIM_DATABASE_WEBUSER'] = 'nobody'
+        monkeypatch.setenv('NOMINATIM_DATABASE_WEBUSER', 'nobody')
 
         config = Configuration(Path(project_dir), DEFCFG_DIR)
 
         assert config.DATABASE_WEBUSER == 'nobody'
 
-        del os.environ['NOMINATIM_DATABASE_WEBUSER']
-
-def test_get_os_env_add_defaults():
+def test_get_os_env_add_defaults(monkeypatch):
     config = Configuration(None, DEFCFG_DIR)
 
-    if 'NOMINATIM_DATABASE_WEBUSER' in os.environ:
-        del os.environ['NOMINATIM_DATABASE_WEBUSER']
+    monkeypatch.delenv('NOMINATIM_DATABASE_WEBUSER', raising=False)
 
     assert config.get_os_env()['NOMINATIM_DATABASE_WEBUSER'] == 'www-data'
 
-def test_get_os_env_prefer_os_environ():
+def test_get_os_env_prefer_os_environ(monkeypatch):
     config = Configuration(None, DEFCFG_DIR)
 
-    os.environ['NOMINATIM_DATABASE_WEBUSER'] = 'nobody'
+    monkeypatch.setenv('NOMINATIM_DATABASE_WEBUSER', 'nobody')
 
     assert config.get_os_env()['NOMINATIM_DATABASE_WEBUSER'] == 'nobody'
-
-    del os.environ['NOMINATIM_DATABASE_WEBUSER']
 
 def test_get_libpq_dsn_convert_default():
     config = Configuration(None, DEFCFG_DIR)
 
     assert config.get_libpq_dsn() == 'dbname=nominatim'
 
-def test_get_libpq_dsn_convert_php():
+def test_get_libpq_dsn_convert_php(monkeypatch):
     config = Configuration(None, DEFCFG_DIR)
 
-    os.environ['NOMINATIM_DATABASE_DSN'] = 'pgsql:dbname=gis;password=foo;host=localhost'
+    monkeypatch.setenv('NOMINATIM_DATABASE_DSN',
+                       'pgsql:dbname=gis;password=foo;host=localhost')
 
     assert config.get_libpq_dsn() == 'dbname=gis password=foo host=localhost'
 
-def test_get_libpq_dsn_convert_libpq():
+def test_get_libpq_dsn_convert_libpq(monkeypatch):
     config = Configuration(None, DEFCFG_DIR)
 
-    os.environ['NOMINATIM_DATABASE_DSN'] = 'host=localhost dbname=gis password=foo'
+    monkeypatch.setenv('NOMINATIM_DATABASE_DSN', 
+                       'host=localhost dbname=gis password=foo')
 
     assert config.get_libpq_dsn() == 'host=localhost dbname=gis password=foo'

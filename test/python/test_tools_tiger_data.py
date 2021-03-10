8486a83cf5972835a585cff4abafd887838b1ecf
Darkshredder <srivastavayash58@gmail.com>
2021-03-10 18:14:17 +0530

    Added test for tarfile

--- test/python/test_tools_tiger_data.py
+++ test/python/test_tools_tiger_data.py
@@ -4,6 +4,7 @@ Test for tiger data function
 from pathlib import Path
 
 import pytest
+import tarfile
 
 from nominatim.tools import tiger_data, database_import
 
@@ -22,4 +23,23 @@ def test_add_tiger_data(dsn, src_dir, def_config, monkeypatch,tmp_path,
     sqlfile.write_text("""INSERT INTO place values (1)""")
     tiger_data.add_tiger_data(dsn, str(tmp_path), threads, def_config, src_dir / 'lib-sql')
 
+    assert temp_db_cursor.table_rows('place') == 1
+
+@pytest.mark.parametrize("threads", (1, 5))
+def test_add_tiger_data_tarfile(dsn, src_dir, def_config, monkeypatch,tmp_path,
+                        temp_db_cursor, threads, temp_db):
+    monkeypatch.setenv('NOMINATIM_DATABASE_MODULE_PATH', '.')
+    temp_db_cursor.execute('CREATE EXTENSION hstore')
+    temp_db_cursor.execute('CREATE EXTENSION postgis')
+    temp_db_cursor.execute('CREATE TABLE place (id INT)')
+
+    database_import.import_base_data('dbname=' + temp_db, src_dir / 'data',
+                                     ignore_partitions=False)
+    sqlfile = tmp_path / '1010.sql'
+    sqlfile.write_text("""INSERT INTO place values (1)""")
+    tar = tarfile.open("sample.tar.gz", "w:gz")
+    tar.add(sqlfile)
+    tar.close()
+    tiger_data.add_tiger_data(dsn, str(src_dir / 'sample.tar.gz'), threads, def_config, src_dir / 'lib-sql')
+    
     assert temp_db_cursor.table_rows('place') == 1

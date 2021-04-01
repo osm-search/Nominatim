8d8b1d4307f7f56c249a4b64b3943e67765d06ee
Sarah Hoffmann <lonvia@denofr.de>
2021-04-01 14:29:34 +0200

    use non-key index to speed up housenumber search
    On Postgresql versions 11+ add an index to speed up the lookup
    of housenumbers for terms found in search_name. This is really
    just a band-aid around the query planer's interpretation of the
    query.

--- nominatim/db/sql_preprocessor.py
+++ nominatim/db/sql_preprocessor.py
@@ -49,11 +49,20 @@ def _setup_postgres_sql(conn):
     pg_version = conn.server_version_tuple()
     # CREATE INDEX IF NOT EXISTS was introduced in PG9.5.
     # Note that you need to ignore failures on older versions when
-    # unsing this construct.
+    # using this construct.
     out['if_index_not_exists'] = ' IF NOT EXISTS ' if pg_version >= (9, 5, 0) else ''
 
     return out
 
+
+def _setup_postgresql_features(conn):
+    """ Set up a dictionary with various optional Postgresql/Postgis features that
+        depend on the database version.
+    """
+    pg_version = conn.server_version_tuple()
+    return {
+        'has_index_non_key_column' : pg_version >= (11, 0, 0)
+    }
 
 class SQLPreprocessor: # pylint: disable=too-few-public-methods
     """ A environment for preprocessing SQL files from the
@@ -79,6 +88,7 @@ class SQLPreprocessor: # pylint: disable=too-few-public-methods
         self.env.globals['config'] = config
         self.env.globals['db'] = db_info
         self.env.globals['sql'] = _setup_postgres_sql(conn)
+        self.env.globals['postgres'] = _setup_postgresql_features(conn)
         self.env.globals['modulepath'] = config.DATABASE_MODULE_PATH or \
                                          str((config.project_dir / 'module').resolve())
 

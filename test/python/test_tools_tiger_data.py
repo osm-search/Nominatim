1f898405a6457115ab6ee232fddfafe14ca367f4
Darkshredder <srivastavayash58@gmail.com>
2021-04-19 15:53:01 +0530

    Fix: tiger-data tarfile test

--- test/python/test_tools_tiger_data.py
+++ test/python/test_tools_tiger_data.py
@@ -39,10 +39,10 @@ def test_add_tiger_data_tarfile(def_config, tmp_path, temp_db_cursor,
     sqlfile = tmp_path / '1010.sql'
     sqlfile.write_text("""INSERT INTO place values (1);
                           INSERT INTO non_existant_table values (1);""")
-    tar = tarfile.open("sample.tar.gz", "w:gz")
+    tar = tarfile.open(str(tmp_path / 'sample.tar.gz'), "w:gz")
     tar.add(sqlfile)
     tar.close()
-    tiger_data.add_tiger_data(str(tmp_path), def_config, threads)
+    tiger_data.add_tiger_data(str(tmp_path / 'sample.tar.gz'), def_config, threads)
 
     assert temp_db_cursor.table_rows('place') == 1
 
@@ -53,9 +53,9 @@ def test_add_tiger_data_bad_tarfile(def_config, tmp_path, temp_db_cursor, thread
     temp_db_cursor.execute('CREATE TABLE place (id INT)')
     sqlfile = tmp_path / '1010.txt'
     sqlfile.write_text("""Random text""")
-    tar = tarfile.open("sample.tar.gz", "w:gz")
+    tar = tarfile.open(str(tmp_path / 'sample.tar.gz'), "w:gz")
     tar.add(sqlfile)
     tar.close()
-    tiger_data.add_tiger_data(str(tmp_path), def_config, threads)
+    tiger_data.add_tiger_data(str(tmp_path / 'sample.tar.gz'), def_config, threads)
 
     assert temp_db_cursor.table_rows('place') == 0

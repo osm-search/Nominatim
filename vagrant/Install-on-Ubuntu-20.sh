8453d88f12bfcdf06b479e40fd18e6af91fece10
Sarah Hoffmann <lonvia@denofr.de>
2021-04-23 20:27:14 +0200

    remove argparse dependency for vagrant scripts
    Users don't need to recreate the manpage.

--- vagrant/Install-on-Ubuntu-20.sh
+++ vagrant/Install-on-Ubuntu-20.sh
@@ -33,8 +33,7 @@ export DEBIAN_FRONTEND=noninteractive #DOCS:
                         postgresql-server-dev-12 postgresql-12-postgis-3 \
                         postgresql-contrib-12 postgresql-12-postgis-3-scripts \
                         php php-pgsql php-intl libicu-dev python3-dotenv \
-                        python3-psycopg2 python3-psutil python3-jinja2 python3-icu git \
-                        python3-argparse-manpage
+                        python3-psycopg2 python3-psutil python3-jinja2 python3-icu git
 
 #
 # System Configuration

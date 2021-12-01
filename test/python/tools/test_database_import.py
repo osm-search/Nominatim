993b238a4169cd527ca5122554a8c52a178200db
Sarah Hoffmann <lonvia@denofr.de>
2021-12-01 11:54:58 +0100

    add more tests for database import

--- test/python/tools/test_database_import.py
+++ test/python/tools/test_database_import.py
@@ -60,6 +60,11 @@ class TestDatabaseSetup:
             database_import.setup_database_skeleton(f'dbname={self.DBNAME}')
 
 
+    def test_create_db_explicit_ro_user(self):
+        database_import.setup_database_skeleton(f'dbname={self.DBNAME}',
+                                                rouser='postgres')
+
+
     def test_create_db_missing_ro_user(self):
         with pytest.raises(UsageError, match='Missing read-only user.'):
             database_import.setup_database_skeleton(f'dbname={self.DBNAME}',
@@ -110,8 +115,15 @@ def test_import_osm_data_multifile(table_factory, tmp_path, osm2pgsql_options, c
 def test_import_osm_data_simple_no_data(table_factory, osm2pgsql_options):
     table_factory('place')
 
-    with pytest.raises(UsageError, match='No data.*'):
+    with pytest.raises(UsageError, match='No data imported'):
         database_import.import_osm_data(Path('file.pbf'), osm2pgsql_options)
+
+
+def test_import_osm_data_simple_ignore_no_data(table_factory, osm2pgsql_options):
+    table_factory('place')
+
+    database_import.import_osm_data(Path('file.pbf'), osm2pgsql_options,
+                                    ignore_errors=True)
 
 
 def test_import_osm_data_drop(table_factory, temp_db_conn, tmp_path, osm2pgsql_options):
@@ -140,11 +152,15 @@ def test_import_osm_data_default_cache(table_factory, osm2pgsql_options, capfd):
     assert f'--cache {osm2pgsql_options["osm2pgsql_cache"]}' in captured.out
 
 
-def test_truncate_database_tables(temp_db_conn, temp_db_cursor, table_factory):
-    tables = ('placex', 'place_addressline', 'location_area',
+@pytest.mark.parametrize("with_search", (True, False))
+def test_truncate_database_tables(temp_db_conn, temp_db_cursor, table_factory, with_search):
+    tables = ['placex', 'place_addressline', 'location_area',
               'location_area_country',
               'location_property_tiger', 'location_property_osmline',
-              'location_postcode', 'search_name', 'location_road_23')
+              'location_postcode', 'location_road_23']
+    if with_search:
+        tables.append('search_name')
+
     for table in tables:
         table_factory(table, content=((1, ), (2, ), (3, )))
         assert temp_db_cursor.table_rows(table) == 3
@@ -159,9 +175,9 @@ def test_truncate_database_tables(temp_db_conn, temp_db_cursor, table_factory):
 def test_load_data(dsn, place_row, placex_table, osmline_table,
                    word_table, temp_db_cursor, threads):
     for func in ('precompute_words', 'getorcreate_housenumber_id', 'make_standard_name'):
-        temp_db_cursor.execute("""CREATE FUNCTION {} (src TEXT)
+        temp_db_cursor.execute(f"""CREATE FUNCTION {func} (src TEXT)
                                   RETURNS TEXT AS $$ SELECT 'a'::TEXT $$ LANGUAGE SQL
-                               """.format(func))
+                               """)
     for oid in range(100, 130):
         place_row(osm_id=oid)
     place_row(osm_type='W', osm_id=342, cls='place', typ='houses',
@@ -171,3 +187,59 @@ def test_load_data(dsn, place_row, placex_table, osmline_table,
 
     assert temp_db_cursor.table_rows('placex') == 30
     assert temp_db_cursor.table_rows('location_property_osmline') == 1
+
+
+class TestSetupSQL:
+
+    @pytest.fixture(autouse=True)
+    def init_env(self, temp_db, tmp_path, def_config, sql_preprocessor_cfg):
+        def_config.lib_dir.sql = tmp_path / 'sql'
+        def_config.lib_dir.sql.mkdir()
+
+        self.config = def_config
+
+
+    def write_sql(self, fname, content):
+        (self.config.lib_dir.sql / fname).write_text(content)
+
+
+    @pytest.mark.parametrize("reverse", [True, False])
+    def test_create_tables(self, temp_db_conn, temp_db_cursor, reverse):
+        self.write_sql('tables.sql',
+                       """CREATE FUNCTION test() RETURNS bool
+                          AS $$ SELECT {{db.reverse_only}} $$ LANGUAGE SQL""")
+
+        database_import.create_tables(temp_db_conn, self.config, reverse)
+
+        temp_db_cursor.scalar('SELECT test()') == reverse
+
+
+    def test_create_table_triggers(self, temp_db_conn, temp_db_cursor):
+        self.write_sql('table-triggers.sql',
+                       """CREATE FUNCTION test() RETURNS TEXT
+                          AS $$ SELECT 'a' $$ LANGUAGE SQL""")
+
+        database_import.create_table_triggers(temp_db_conn, self.config)
+
+        temp_db_cursor.scalar('SELECT test()') == 'a'
+
+
+    def test_create_partition_tables(self, temp_db_conn, temp_db_cursor):
+        self.write_sql('partition-tables.src.sql',
+                       """CREATE FUNCTION test() RETURNS TEXT
+                          AS $$ SELECT 'b' $$ LANGUAGE SQL""")
+
+        database_import.create_partition_tables(temp_db_conn, self.config)
+
+        temp_db_cursor.scalar('SELECT test()') == 'b'
+
+
+    @pytest.mark.parametrize("drop", [True, False])
+    def test_create_search_indices(self, temp_db_conn, temp_db_cursor, drop):
+        self.write_sql('indices.sql',
+                       """CREATE FUNCTION test() RETURNS bool
+                          AS $$ SELECT {{drop}} $$ LANGUAGE SQL""")
+
+        database_import.create_search_indices(temp_db_conn, self.config, drop)
+
+        temp_db_cursor.scalar('SELECT test()') == drop

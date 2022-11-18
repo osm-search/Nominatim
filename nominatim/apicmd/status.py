45c675bd786ff70c1417fc8e92cff8d3a2ad6e75
Sarah Hoffmann <lonvia@denofr.de>
2022-11-18 16:11:31 +0100

    implement command line status call in Python

---
+++ nominatim/apicmd/status.py
@@ -0,0 +1,66 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# This file is part of Nominatim. (https://nominatim.org)
+#
+# Copyright (C) 2022 by the Nominatim developer community.
+# For a full list of authors see the git log.
+"""
+Classes and function releated to status call.
+"""
+from typing import Optional, cast
+import datetime as dt
+
+import sqlalchemy as sqla
+from sqlalchemy.ext.asyncio.engine import AsyncEngine, AsyncConnection
+import asyncpg
+
+from nominatim import version
+
+class StatusResult:
+    """ Result of a call to the status API.
+    """
+
+    def __init__(self, status: int, msg: str):
+        self.status = status
+        self.message = msg
+        # XXX versions really should stay tuples here
+        self.software_version = version.version_str()
+        self.data_updated: Optional[dt.datetime]  = None
+        self.database_version: Optional[str] = None
+
+
+async def _get_database_date(conn: AsyncConnection) -> Optional[dt.datetime]:
+    """ Query the database date.
+    """
+    sql = sqla.text('SELECT lastimportdate FROM import_status LIMIT 1')
+    result = await conn.execute(sql)
+
+    for row in result:
+        return cast(dt.datetime, row[0])
+
+    return None
+
+
+async def _get_database_version(conn: AsyncConnection) -> Optional[str]:
+    sql = sqla.text("""SELECT value FROM nominatim_properties
+                       WHERE property = 'database_version'""")
+    result = await conn.execute(sql)
+
+    for row in result:
+        return cast(str, row[0])
+
+    return None
+
+
+async def get_status(engine: AsyncEngine) -> StatusResult:
+    """ Execute a status API call.
+    """
+    status = StatusResult(0, 'OK')
+    try:
+        async with engine.begin() as conn:
+            status.data_updated = await _get_database_date(conn)
+            status.database_version = await _get_database_version(conn)
+    except asyncpg.PostgresError as err:
+        return StatusResult(700, str(err))
+
+    return status
---
+++ nominatim/result_formatter/v1.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# This file is part of Nominatim. (https://nominatim.org)
+#
+# Copyright (C) 2022 by the Nominatim developer community.
+# For a full list of authors see the git log.
+"""
+Output formatters for API version v1.
+"""
+from typing import Dict, Any
+from collections import OrderedDict
+import json
+
+from nominatim.result_formatter.base import FormatDispatcher
+from nominatim.apicmd.status import StatusResult
+
+create = FormatDispatcher()
+
+@create.format_func(StatusResult, 'text')
+def _format_status_text(result: StatusResult) -> str:
+    return result.message
+
+
+@create.format_func(StatusResult, 'json')
+def _format_status_json(result: StatusResult) -> str:
+    # XXX write a simple JSON serializer
+    out: Dict[str, Any] = OrderedDict()
+    out['status'] = result.status
+    out['message'] = result.message
+    if result.data_updated is not None:
+        out['data_updated'] = result.data_updated
+    out['software_version'] = result.software_version
+    if result.database_version is not None:
+        out['database_version'] = result.database_version
+
+    return json.dumps(out)

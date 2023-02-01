370c9b38c096468abb84fa29f605843cdeb9f93e
Sarah Hoffmann <lonvia@denofr.de>
2023-02-01 10:22:53 +0100

    improve scaffolding for API unit tests
    Use the static table definition to create the test database.
    Add helper function to simplify filling the tables.

--- test/python/api/conftest.py
+++ test/python/api/conftest.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Nominatim. (https://nominatim.org)
 #
-# Copyright (C) 2022 by the Nominatim developer community.
+# Copyright (C) 2023 by the Nominatim developer community.
 # For a full list of authors see the git log.
 """
 Helper fixtures for API call tests.
@@ -11,12 +11,44 @@ from pathlib import Path
 import pytest
 import time
 
-from nominatim.api import NominatimAPI
+import nominatim.api as napi
+
+class APITester:
+
+    def __init__(self):
+        self.api = napi.NominatimAPI(Path('/invalid'), {})
+        self.async_to_sync(self.api._async_api.setup_database())
+
+
+    def async_to_sync(self, func):
+        """ Run an asynchronous function until completion using the
+            internal loop of the API.
+        """
+        return self.api._loop.run_until_complete(func)
+
+
+    def add_data(self, table, data):
+        """ Insert data into the given table.
+        """
+        sql = getattr(self.api._async_api._tables, table).insert()
+        self.async_to_sync(self.exec_async(sql, data))
+
+
+    async def exec_async(self, sql, *args, **kwargs):
+        async with self.api._async_api.begin() as conn:
+            return await conn.execute(sql, *args, **kwargs)
+
+
+    async def create_tables(self):
+        async with self.api._async_api._engine.begin() as conn:
+            await conn.run_sync(self.api._async_api._tables.meta.create_all)
+
 
 @pytest.fixture
-def apiobj(temp_db):
+def apiobj(temp_db_with_extensions):
     """ Create an asynchronous SQLAlchemy engine for the test DB.
     """
-    api = NominatimAPI(Path('/invalid'), {})
-    yield api
-    api.close()
+    testapi = APITester()
+    testapi.async_to_sync(testapi.create_tables())
+    yield testapi
+    testapi.api.close()

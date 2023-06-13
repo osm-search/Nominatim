e304e5f72d9e9c4514499e458f91492e9cff9917
miku0 <miku.f.810129@gmail.com>
2023-06-13 10:11:56 +0000

    write japanese test (error)

--- test/python/tokenizer/sanitizers/test_tag_japanese.py
+++ test/python/tokenizer/sanitizers/test_tag_japanese.py
@@ -12,28 +12,34 @@ class TestTagJapanese:
         self.config = def_config
 
     def run_sanitizer_on(self, type, **kwargs):
-
-        place = PlaceInfo({type: {k.replace('_', ':'): v for k, v in kwargs.items()},
-                            'country_code': 'ja'})
+        place = PlaceInfo({
+            'address': [
+                PlaceName(kind='housenumber', name=kwargs.get('housenumber')),
+                PlaceName(kind='block_number', name=kwargs.get('block_number')),
+                PlaceName(kind='neighbourhood', name=kwargs.get('neighbourhood')),
+            ],
+            'country_code': 'ja'
+        })
 
         sanitizer_args = {'step': 'tag-japanese'}
 
-        name, address = PlaceSanitizer([sanitizer_args],
-                                    self.config).process_names(place)
+        _, address = PlaceSanitizer([sanitizer_args], self.config).process_names(place)
 
-        return {
-                'name': sorted([(p.name, p.kind, p.suffix or '') for p in name]),
-                'address': sorted([(p.name, p.kind, p.suffix or '') for p in address])
-            }
-
-
-    def test_on_name(self):
-        res = self.run_sanitizer_on('name', name='foo', ref='bar', ref_abc='baz')
-
-        assert res.get('name') == []
+        return sorted([(p.kind, p.name) for p in address])
 
     def test_on_address(self):
-        res = self.run_sanitizer_on('address', name='foo', ref='bar', ref_abc='baz')
+        res = self.run_sanitizer_on(name='foo', ref='bar', ref_abc='baz')
+        assert res == [('bar', 'ref'), ('baz', 'ref', 'abc'), ('foo', 'name')]
 
-        assert res.get('address') == [('bar', 'ref', ''), ('baz', 'ref', 'abc'),
-                                        ('foo', 'name', '')]
+    def test_tag_without_housenumber_and_blocknumber(self):
+        obj = PlaceInfo()
+        obj.address = [
+            PlaceName(kind='neighbourhood', name='2丁目')
+        ]
+        self.run_sanitizer_on(obj)
+
+        assert [(p.kind, p.name) for p in obj.address] == [('place', '2丁目')]
+
+    def test_housenumber_type(self):
+        res = self.run_sanitizer_on(housenumber='2', block_number='6')
+        assert res == [('housenumber', '6-2')]

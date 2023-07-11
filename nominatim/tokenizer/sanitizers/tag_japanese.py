d84145723a325419abb1d5254b452d1d7489c580
miku0 <miku.f.810129@gmail.com>
2023-07-11 12:48:04 +0000

    fix the error of mypy and pylint

--- nominatim/tokenizer/sanitizers/tag_japanese.py
+++ nominatim/tokenizer/sanitizers/tag_japanese.py
@@ -1,14 +1,30 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# This file is part of Nominatim. (https://nominatim.org)
+#
+# Copyright (C) 2022 by the Nominatim developer community.
+# For a full list of authors see the git log.
+"""
+This sanitizer maps OSM data to Japanese block addresses.
+It replaces blocknumber and housenumber with housenumber,
+and quarter and neighbourhood with place.
+"""
+
+
 from typing import Callable
-import re
 
 from nominatim.tokenizer.sanitizers.base import ProcessInfo
 from nominatim.tokenizer.sanitizers.config import SanitizerConfig
 from nominatim.data.place_name import PlaceName
 
-def create(config: SanitizerConfig) -> None:
+def create(config: SanitizerConfig) -> Callable[[ProcessInfo],None]:
+    '''Set up the sanitizer
+    '''
     return tag_japanese
 
 def convert_kanji_sequence_to_number(sequence: str) -> str:
+    '''Converts Kanji numbers to Arabic numbers
+    '''
     kanji_map = {
       '零': '0',
       '一': '1',
@@ -24,17 +40,19 @@ def convert_kanji_sequence_to_number(sequence: str) -> str:
     converted = ''
     current_number = ''
     for char in sequence:
-      if char in kanji_map:
-        current_number += kanji_map[char]
-      else:
-        converted += current_number
-        current_number = ''
-        converted += char
+        if char in kanji_map:
+            current_number += kanji_map[char]
+        else:
+            converted += current_number
+            current_number = ''
+            converted += char
     converted += current_number
     return converted
 
 
 def tag_japanese(obj: ProcessInfo) -> None:
+    '''Recombine kind of address
+    '''
     if obj.place.country_code != 'jp':
         return
     tmp_housenumber = None
@@ -44,7 +62,7 @@ def tag_japanese(obj: ProcessInfo) -> None:
 
     new_address = []
     for item in obj.names:
-      item.name = convert_kanji_sequence_to_number(item.name) 
+        item.name = convert_kanji_sequence_to_number(item.name)
 
     for item in obj.address:
         item.name = convert_kanji_sequence_to_number(item.name)
@@ -60,17 +78,53 @@ def tag_japanese(obj: ProcessInfo) -> None:
             new_address.append(item)
 
     if tmp_blocknumber and tmp_housenumber:
-        new_address.append(PlaceName(kind='housenumber', name=f'{tmp_blocknumber}-{tmp_housenumber}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='housenumber',
+                name=f'{tmp_blocknumber}-{tmp_housenumber}',
+                suffix=''
+            )
+        )
     elif tmp_blocknumber:
-        new_address.append(PlaceName(kind='housenumber', name=f'{tmp_blocknumber}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='housenumber',
+                name=f'{tmp_blocknumber}',
+                suffix=''
+            )
+        )
     elif tmp_housenumber:
-        new_address.append(PlaceName(kind='housenumber', name=f'{tmp_housenumber}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='housenumber',
+                name=f'{tmp_housenumber}',
+                suffix=''
+            )
+        )
 
     if tmp_neighbourhood and tmp_quarter:
-        new_address.append(PlaceName(kind='place', name=f'{tmp_quarter}{tmp_neighbourhood}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='place',
+                name=f'{tmp_quarter}{tmp_neighbourhood}',
+                suffix=''
+            )
+        )
     elif tmp_neighbourhood:
-        new_address.append(PlaceName(kind='place', name=f'{tmp_neighbourhood}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='place',
+                name=f'{tmp_neighbourhood}',
+                suffix=''
+            )
+        )
     elif tmp_quarter:
-        new_address.append(PlaceName(kind='place', name=f'{tmp_quarter}',suffix=''))
+        new_address.append(
+            PlaceName(
+                kind='place',
+                name=f'{tmp_quarter}',
+                suffix=''
+            )
+        )
 
     obj.address = [item for item in new_address if item.name is not None]

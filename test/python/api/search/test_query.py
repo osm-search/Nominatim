870647980d3947b0a1fcd4c7f4ead6771dec3535
Jithendra Palepu <jithendra@fsfe.org>
2023-07-27 15:40:12 +0200

    Adding spdx license and copyright comment headers

--- test/python/api/search/test_query.py
+++ test/python/api/search/test_query.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023 Nominatim developer community <https://nominatim.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
--- test/python/utils/test_json_writer.py
+++ test/python/utils/test_json_writer.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Nominatim developer community <https://nominatim.org>
+#
+# SPDX-License-Identifier: GPL-3.0-later
+
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 # This file is part of Nominatim. (https://nominatim.org)

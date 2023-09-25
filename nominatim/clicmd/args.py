01289227f887adbaac810460261f1c566c01bb7d
Robbe Haesendonck <googleit@inuits.eu>
2023-09-25 11:09:20 +0200

    Renamed flags

--- nominatim/clicmd/args.py
+++ nominatim/clicmd/args.py
@@ -86,8 +86,8 @@ class NominatimArgs:
     offline: bool
     ignore_errors: bool
     index_noanalyse: bool
-    no_superuser: bool
-    prepare_database: bool
+    only_import_data: bool
+    only_prepare_database: bool
 
     # Arguments to 'index'
     boundaries_only: bool

22bd84524a7d8e97e7de5def0588cabc8df2a525
anqixxx <anqix2002@gmail.com>
2025-04-19 13:11:43 -0700

    remove mock database test

--- test/python/tools/test_sp_importer.py
+++ test/python/tools/test_sp_importer.py
@@ -1,41 +1,5 @@
 from nominatim_db.tools.special_phrases.sp_importer import SPImporter
 
-
-# Testing Database Class Pair Retrival using Mock Database
-def test_get_classtype_pairs(monkeypatch):
-    class Config:
-        def load_sub_configuration(self, path, section=None):
-            return {"blackList": {}, "whiteList": {}}
-
-    class Cursor:
-        def __enter__(self): return self
-        def __exit__(self, exc_type, exc_val, exc_tb): pass
-
-        def fetchall(self):
-            return [
-                ("highway", "motorway"),
-                ("historic", "castle")
-            ]
-
-        def execute(self, query): pass
-
-    class Connection:
-        def cursor(self): return Cursor()
-
-    config = Config()
-    conn = Connection()
-    importer = SPImporter(config=config, conn=conn, sp_loader=None)
-
-    result = importer.get_classtype_pairs()
-
-    expected = {
-        ("highway", "motorway"),
-        ("historic", "castle")
-    }
-
-    assert result == expected
-
-
 # Testing Database Class Pair Retrival using Conftest.py and placex
 def test_get_classtype_pair_data(placex_table, temp_db_conn):
     class Config:

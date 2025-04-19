64cb68a1035000a8541eb89d449722a56fbf853c
anqixxx <anqix2002@gmail.com>
2025-04-19 13:03:01 -0700

    flaking for test file

--- test/python/tools/test_sp_importer.py
+++ test/python/tools/test_sp_importer.py
@@ -1,8 +1,5 @@
-import pytest
-import tempfile
-import os
-
 from nominatim_db.tools.special_phrases.sp_importer import SPImporter
+
 
 # Testing Database Class Pair Retrival using Mock Database
 def test_get_classtype_pairs(monkeypatch):
@@ -11,14 +8,16 @@ def test_get_classtype_pairs(monkeypatch):
             return {"blackList": {}, "whiteList": {}}
 
     class Cursor:
-        def execute(self, query): pass
-        def fetchall(self):
-            return [
-                ("highway", "motorway"),  
-                ("historic", "castle")    
-            ]
         def __enter__(self): return self
         def __exit__(self, exc_type, exc_val, exc_tb): pass
+
+        def fetchall(self):
+            return [
+                ("highway", "motorway"),
+                ("historic", "castle")
+            ]
+
+        def execute(self, query): pass
 
     class Connection:
         def cursor(self): return Cursor()
@@ -36,17 +35,18 @@ def test_get_classtype_pairs(monkeypatch):
 
     assert result == expected
 
+
 # Testing Database Class Pair Retrival using Conftest.py and placex
 def test_get_classtype_pair_data(placex_table, temp_db_conn):
     class Config:
         def load_sub_configuration(self, *_):
             return {'blackList': {}, 'whiteList': {}}
-        
+
     for _ in range(101):
-        placex_table.add(cls='highway', typ='motorway') # edge case 101
+        placex_table.add(cls='highway', typ='motorway')  # edge case 101
 
     for _ in range(99):
-        placex_table.add(cls='amenity', typ='prison') # edge case 99
+        placex_table.add(cls='amenity', typ='prison')  # edge case 99
 
     for _ in range(150):
         placex_table.add(cls='tourism', typ='hotel')
@@ -63,16 +63,17 @@ def test_get_classtype_pair_data(placex_table, temp_db_conn):
 
     assert result == expected, f"Expected {expected}, got {result}"
 
+
 def test_get_classtype_pair_data_more(placex_table, temp_db_conn):
     class Config:
         def load_sub_configuration(self, *_):
             return {'blackList': {}, 'whiteList': {}}
-        
+
     for _ in range(100):
-        placex_table.add(cls='emergency', typ='firehydrant') # edge case 100, not included
+        placex_table.add(cls='emergency', typ='firehydrant')  # edge case 100, not included
 
     for _ in range(199):
-        placex_table.add(cls='amenity', typ='prison') 
+        placex_table.add(cls='amenity', typ='prison')
 
     for _ in range(3478):
         placex_table.add(cls='tourism', typ='hotel')
@@ -94,12 +95,12 @@ def test_get_classtype_pair_data_default(placex_table, temp_db_conn):
     class Config:
         def load_sub_configuration(self, *_):
             return {'blackList': {}, 'whiteList': {}}
-        
+
     for _ in range(1):
         placex_table.add(cls='emergency', typ='firehydrant')
 
     for _ in range(199):
-        placex_table.add(cls='amenity', typ='prison') 
+        placex_table.add(cls='amenity', typ='prison')
 
     for _ in range(3478):
         placex_table.add(cls='tourism', typ='hotel')
@@ -116,4 +117,3 @@ def test_get_classtype_pair_data_default(placex_table, temp_db_conn):
     }
 
     assert result == expected, f"Expected {expected}, got {result}"
-

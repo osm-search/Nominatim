047f3e49ac795fb5c056aa9eae7db3beb47288a0
anqixxx <anqix2002@gmail.com>
2025-08-25 19:26:03 -0700

    Lint

--- src/nominatim_api/localization/transliterator.py
+++ src/nominatim_api/localization/transliterator.py
@@ -12,7 +12,7 @@ from ..data import lang_info, country_info
 
 # optional dependencies
 try:
-    from unidecode import unidecode
+    from unidecode import unidecode  # type: ignpre
 except ImportError:
     unidecode = None
 try:

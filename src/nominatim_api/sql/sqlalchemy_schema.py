776c8bc9b0e98aa9803c016ae138c8f2e461f3da
Sakina <sakinaismail1918@gmail.com>
2026-03-12 12:45:10 -0400

    Synching SqlAlchemy schema with NOT Null issues and constraints

--- src/nominatim_api/sql/sqlalchemy_schema.py
+++ src/nominatim_api/sql/sqlalchemy_schema.py
@@ -38,11 +38,12 @@ class SearchTables:
             sa.Column('place_id', sa.BigInteger, nullable=False),
             sa.Column('parent_place_id', sa.BigInteger),
             sa.Column('linked_place_id', sa.BigInteger),
-            sa.Column('importance', sa.Float),
+            sa.Column('importance', sa.Float, nullable=False),
             sa.Column('indexed_date', sa.DateTime),
-            sa.Column('rank_address', sa.SmallInteger),
-            sa.Column('rank_search', sa.SmallInteger),
-            sa.Column('indexed_status', sa.SmallInteger),
+            sa.Column('rank_address', sa.SmallInteger, nullable=False),
+            sa.Column('rank_search', sa.SmallInteger, nullable=False),
+            sa.Column('partition', sa.SmallInteger, nullable=False),
+            sa.Column('indexed_status', sa.SmallInteger, nullable=False),
             sa.Column('osm_type', sa.String(1), nullable=False),
             sa.Column('osm_id', sa.BigInteger, nullable=False),
             sa.Column('class', sa.Text, nullable=False, key='class_'),
@@ -56,40 +57,42 @@ class SearchTables:
             sa.Column('country_code', sa.String(2)),
             sa.Column('housenumber', sa.Text),
             sa.Column('postcode', sa.Text),
-            sa.Column('centroid', Geometry))
+                       
+            
+            )
 
         self.addressline = sa.Table(
             'place_addressline', meta,
             sa.Column('place_id', sa.BigInteger),
-            sa.Column('address_place_id', sa.BigInteger),
-            sa.Column('distance', sa.Float),
-            sa.Column('fromarea', sa.Boolean),
-            sa.Column('isaddress', sa.Boolean))
+            sa.Column('address_place_id', sa.BigInteger, nullable=False),
+            sa.Column('distance', sa.Float, nullable=False),
+            sa.Column('fromarea', sa.Boolean, nullable=False),
+            sa.Column('isaddress', sa.Boolean, nullable=False))
 
         self.postcode = sa.Table(
             'location_postcodes', meta,
-            sa.Column('place_id', sa.BigInteger),
+            sa.Column('place_id', sa.BigInteger, nullable=False),
             sa.Column('parent_place_id', sa.BigInteger),
             sa.Column('osm_id', sa.BigInteger),
-            sa.Column('rank_search', sa.SmallInteger),
-            sa.Column('indexed_status', sa.SmallInteger),
+            sa.Column('rank_search', sa.SmallInteger, nullable=False),
+            sa.Column('indexed_status', sa.SmallInteger, nullable=False),
             sa.Column('indexed_date', sa.DateTime),
-            sa.Column('country_code', sa.String(2)),
-            sa.Column('postcode', sa.Text),
-            sa.Column('centroid', Geometry),
-            sa.Column('geometry', Geometry))
+            sa.Column('country_code', sa.String(2), nullable=False),
+            sa.Column('postcode', sa.Text, nullable=False),
+            sa.Column('centroid', Geometry, nullable=False),
+            sa.Column('geometry', Geometry, nullable=False))
 
         self.osmline = sa.Table(
             'location_property_osmline', meta,
             sa.Column('place_id', sa.BigInteger, nullable=False),
-            sa.Column('osm_id', sa.BigInteger),
+            sa.Column('osm_id', sa.BigInteger, nullable=False),
             sa.Column('parent_place_id', sa.BigInteger),
             sa.Column('indexed_date', sa.DateTime),
             sa.Column('startnumber', sa.Integer),
             sa.Column('endnumber', sa.Integer),
             sa.Column('step', sa.SmallInteger),
-            sa.Column('indexed_status', sa.SmallInteger),
-            sa.Column('linegeo', Geometry),
+            sa.Column('indexed_status', sa.SmallInteger, nullable=False),
+            sa.Column('linegeo', Geometry, nullable=False),
             sa.Column('address', KeyValueStore),
             sa.Column('postcode', sa.Text),
             sa.Column('country_code', sa.String(2)))
@@ -110,28 +113,28 @@ class SearchTables:
         # The following tables are not necessarily present.
         self.search_name = sa.Table(
             'search_name', meta,
-            sa.Column('place_id', sa.BigInteger),
-            sa.Column('importance', sa.Float),
-            sa.Column('address_rank', sa.SmallInteger),
-            sa.Column('name_vector', IntArray),
-            sa.Column('nameaddress_vector', IntArray),
+            sa.Column('place_id', sa.BigInteger, nullable=False),
+            sa.Column('importance', sa.Float, nullable=False),
+            sa.Column('address_rank', sa.SmallInteger, nullable=False),
+            sa.Column('name_vector', IntArray, nullable=False),
+            sa.Column('nameaddress_vector', IntArray, nullable=False),
             sa.Column('country_code', sa.String(2)),
-            sa.Column('centroid', Geometry))
+            sa.Column('centroid', Geometry, nullable=False))
 
         self.tiger = sa.Table(
             'location_property_tiger', meta,
-            sa.Column('place_id', sa.BigInteger),
+            sa.Column('place_id', sa.BigInteger, nullable=False),
             sa.Column('parent_place_id', sa.BigInteger),
-            sa.Column('startnumber', sa.Integer),
-            sa.Column('endnumber', sa.Integer),
-            sa.Column('step', sa.SmallInteger),
-            sa.Column('linegeo', Geometry),
-            sa.Column('postcode', sa.Text))
+            sa.Column('startnumber', sa.Integer, nullable=False),
+            sa.Column('endnumber', sa.Integer, nullable=False),
+            sa.Column('step', sa.SmallInteger, nullable=False),
+            sa.Column('linegeo', Geometry, nullable=False),
+            sa.Column('postcode', sa.Text, nullable=False))
 
         self.placex_entrance = sa.Table(
             'placex_entrance', meta,
-            sa.Column('place_id', sa.BigInteger),
-            sa.Column('osm_id', sa.BigInteger),
-            sa.Column('type', sa.Text),
+            sa.Column('place_id', sa.BigInteger, nullable=False),
+            sa.Column('osm_id', sa.BigInteger, nullable=False),
+            sa.Column('type', sa.Text, nullable=False),
             sa.Column('location', Geometry, nullable=False),
             sa.Column('extratags', KeyValueStore))

ba9d6c4d247b94acc76b11882da94c0d4d156932
Ayush Dhar Dubey <mailaddubey@gmail.com>
2026-06-22 02:59:19 +0530

    fix regex: allow backward slash

--- lib-lua/flex-base.lua
+++ lib-lua/flex-base.lua
@@ -2,7 +2,7 @@
 local flex = require('themes/nominatim/init')
 
 function flex.load_topic(name, cfg)
-    local topic_file = debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. 'themes/nominatim/topics/'.. name .. '.lua'
+    local topic_file = debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])") .. 'themes/nominatim/topics/'.. name .. '.lua'
 
     if topic_file == nil then
         error('Cannot find topic: ' .. name)
